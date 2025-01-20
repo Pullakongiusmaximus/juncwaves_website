5f5455d5d2ff02c685534af99b81c07c5c376f3f
Pullakongiusmaximus <kapklim@gmail.com>
2025-01-20 21:49:05 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,30 +4,30 @@ var klaroConfig = {
 
     },
     services: [
-    {
-     name: 'matomo',
-     default: true,
-     title: 'Matomo',
-     purposes: ['analytics'],
-     cookies: [
-         [/^_pk_.*$/, '/', 'localhost'],
-         [/^_mtm_.*$/, '/', 'localhost'],
-         [/^MATOMO.*$/, '/', 'localhost'],
-     ],
-     callback: function(consent, service) {
-         if (_paq !== 'undefined') {}
-             if (consent == true){
-                  _paq.push(['rememberCookieConsentGiven']);
-                  _paq.push(['setConsentGiven']);
-             } else {
-                  _paq.push(['forgetCookieConsentGiven'])
-                  _paq.push(['deleteCookies']);
-             }
-          }
-      },
-      required: false,
-      optOut: false,
-      onlyOnce: true,
-     },
+        {
+            name: 'matomo',
+            default: true,
+            title: 'Matomo',
+            purposes: ['analytics'],
+            cookies: [
+                [/^_pk_.*$/, '/', 'localhost'],
+                [/^_mtm_.*$/, '/', 'localhost'],
+                [/^MATOMO.*$/, '/', 'localhost']
+            ],
+            callback: function(consent, service) {
+                if (typeof _paq !== 'undefined') {  
+                    if (consent === true) {
+                        _paq.push(['rememberCookieConsentGiven']);
+                        _paq.push(['setConsentGiven']);
+                    } else {
+                        _paq.push(['forgetCookieConsentGiven']);
+                        _paq.push(['deleteCookies']); 
+                    }
+                }  
+            },
+            required: false,
+            optOut: false,
+            onlyOnce: true
+        }  
     ]
 };
